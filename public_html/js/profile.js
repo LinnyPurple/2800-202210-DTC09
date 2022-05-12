c6690b5aa80072be207653fd47e3107ac76b9898
Soohyeun <soohyeunp@gmail.com>
2022-05-12 13:37:27 -0700

    Implement the function populate user's information in profile page'

--- public_html/js/profile.js
+++ public_html/js/profile.js
@@ -1,8 +1,33 @@
-// Todo: Populate userinfo
-// async function getUserInfo() {
-//     let res = await getRequest("/api/getUserInfo");
-//     console.log(res);
-// }
+// Populate userinfo
+async function getUserInfo() {
+    let res = await getRequest("/api/getUserInfo");
+    console.log(res);
+    var currentUserInfo = JSON.parse(res);
+
+    // Check user is logged in
+    if (currentUserInfo.loggedIn == true) {
+        console.log("User is logged in")
+        var currentUsername = currentUserInfo.name;
+        var currentUserEmail = currentUserInfo.email;
+        // var currentUserLocation = currentUserInfo.location;
+
+        if (currentUsername != null) {
+            document.getElementById("nameInput").value = currentUsername;
+        }
+
+        if (currentUserEmail != null) {
+        document.getElementById("emailInput").value = currentUserEmail;
+        }
+
+        // if (currentUserLocation != null) {
+        //     document.getElementById("locationInput").value = currentUserLocation;
+        //     }
+
+    } else {
+        console.log("No user is signed in");
+        window.location.href = "./login.html";
+    }
+}
 
 //enable the fields for value change
 function editUserInfo() {
@@ -28,8 +53,26 @@ function editUserInfo() {
 
 // }
 
+// async function createAccountV2(username = "testuser", email = "testEmail", password = "testPass") {
+//     let res = await postRequest("/api/createAccount", {
+//         password: password,
+//         username: username,
+//         email: email,
+//         accessLevel: 1
+//     });
+//     console.log(res);
+// }
+
+// async function login(username = "testuser") {
+//     let res = await postRequest("/api/login", {
+//         password: "testPass",
+//         username: username
+//     });
+//     console.log(res);
+// }
+
 function setup() {
-    // getUserInfo()
+    getUserInfo()
 }
 
 $(document).ready(setup)
