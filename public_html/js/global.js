faf5650ae165d92e4655e004cac45bd54ffa72f6
Toco Tachibana <ttachibana@my.bcit.ca>
2022-05-17 23:46:23 -0700

    style: fix font size for mobile display

--- public_html/js/global.js
+++ public_html/js/global.js
@@ -1,6 +1,6 @@
 // inject partials
 (() => {
-  $(".top-nav").load('/html/partials/top-nav.html');
+  $("header").load('/html/partials/top-nav.html');
   $(".bottom-nav").load('/html/partials/menu-bar.html');
 })
 ();
