e0f9c9f0544d0db172a5527ec887bf15d88af91c
Toco Tachibana <ttachibana@my.bcit.ca>
2022-05-18 01:31:19 -0700

    feat: link CSS and JS files to signup.html

--- public_html/js/global.js
+++ public_html/js/global.js
@@ -1,7 +1,7 @@
 // inject partials
 (() => {
   $("header").load('/html/partials/top-nav.html');
-  $(".bottom-nav").load('/html/partials/menu-bar.html');
+  $("footer").load('/html/partials/menu-bar.html');
 })
 ();
 
