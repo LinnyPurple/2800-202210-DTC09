e9a7f480d0bf186ae60b684709060b5c98b7b2ce
Soohyeun <soohyeunp@gmail.com>
2022-05-18 18:16:44 -0700

    Complete design in posting page

--- public_html/js/posting.js
+++ public_html/js/posting.js
@@ -1,18 +1,26 @@
 // check user logged in
-async function getUserInfo() {
-    let res = await getRequest("/api/getUserInfo");
+// async function getUserInfo() {
+//     let res = await getRequest("/api/getUserInfo");
+//     console.log(res);
+//     var currentUserInfo = JSON.parse(res);
+
+//     // Check user is logged in
+//     if (currentUserInfo.loggedIn == true) {
+//         console.log("User is logged in")
+//         //function name!!!
+
+//     } else {
+//         console.log("No user is signed in");
+//         window.location.href = "/login";
+//     }
+// }
+
+async function login(username) {
+    let res = await postRequest("/api/login", {
+        password: "a",
+        username: username
+    });
     console.log(res);
-    var currentUserInfo = JSON.parse(res);
-
-    // Check user is logged in
-    if (currentUserInfo.loggedIn == true) {
-        console.log("User is logged in")
-        //function name!!!
-
-    } else {
-        console.log("No user is signed in");
-        window.location.href = "/login";
-    }
 }
 
 // upload picture
